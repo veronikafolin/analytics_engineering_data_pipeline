51b9152b1ab5616edaf5ea12a1eab2886ec3f6bd
Veronika Folin <veronika.folin@studio.unibo.it>
2023-11-08 09:03:31 +0100

    refactor: code indentation

--- macros/generic/compute_volume_orders.sql
+++ macros/generic/compute_volume_orders.sql
@@ -8,7 +8,8 @@ select
     max(orderdate) as most_recent_order_date,
     count(orderkey) as number_of_orders,
     sum(totalprice) as sum_total_price
-from {{ref('fct_orders')}}
+from
+    {{ref('fct_orders')}}
 group by
     {% for key, value in kwargs.items() %}
     {{value}}{% if not loop.last %},{% endif %}
