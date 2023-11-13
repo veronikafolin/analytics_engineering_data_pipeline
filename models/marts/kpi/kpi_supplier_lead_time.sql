50031c447c6db0235adb588bd0052fc336c4028e
Veronika Folin <veronika.folin@studio.unibo.it>
2023-11-13 16:59:50 +0100

    feat: KPI to compute the supplier_lead_time

---
+++ models/marts/kpi/kpi_supplier_lead_time.sql
@@ -0,0 +1,20 @@
+with
+
+sales as (
+    select * from {{ref('fct_sales')}}
+),
+
+delivery_time as (
+    select
+        shipdate,
+        receiptdate,
+        datediff(day, shipdate, receiptdate) as delivery_days
+    from sales
+),
+
+final as (
+    select avg(delivery_days)
+    from delivery_time
+)
+
+select * from final
