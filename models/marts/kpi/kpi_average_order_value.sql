4ecca211c6782850b4dbf71d6bf84432ed9c2eb7
Veronika Folin <veronika.folin@studio.unibo.it>
2023-11-13 15:02:24 +0100

    refactor KPIs

--- models/marts/kpi/kpi_average_order_value.sql
+++ models/marts/kpi/kpi_average_order_value.sql
@@ -1,7 +1,33 @@
+{% set groupBycolumns = var("groupBy") %}
+{% set filters = var("filters") %}
+
 with
 
+orders as (
+    select * from {{ref('stg_orders')}}
+),
+
 final as (
-    {{ compute_average_order_value() }}
+
+    select
+        {% for col in groupBycolumns %}
+        {{col}},
+        {% endfor %}
+        sum(totalprice)/count(orderkey) as average_order_value
+
+    from orders
+
+    {% for filter in filters %}
+    where {{filter["field"]}} = '{{filter["value"]}}'
+    {% endfor %}
+
+    {% if groupBycolumns|length > 0 %}
+        group by
+            {% for col in groupBycolumns %}
+            {{col}}{% if not loop.last %},{% endif %}
+            {% endfor %}
+    {% endif %}
+
 )
 
 select * from final
--- models/marts/kpi/kpi_revenue.sql
+++ models/marts/kpi/kpi_revenue.sql
@@ -1,7 +1,30 @@
+{% set groupBycolumns = var("groupBy") %}
+{% set filters = var("filters") %}
+
 with
 
+orders as (
+    select * from {{ref('stg_orders')}}
+),
+
 final as (
-    {{ compute_revenue() }}
+
+    select
+        {% for col in groupBycolumns %}
+        {{col}},
+        {% endfor %}
+        sum(totalprice) as total_revenue
+    from orders
+    {% for filter in filters %}
+    where {{filter["field"]}} = '{{filter["value"]}}'
+    {% endfor %}
+    {% if groupBycolumns|length > 0 %}
+        group by
+            {% for col in groupBycolumns %}
+            {{col}}{% if not loop.last %},{% endif %}
+            {% endfor %}
+    {% endif %}
+
 )
 
 select * from final
