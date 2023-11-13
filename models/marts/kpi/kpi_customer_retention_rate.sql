dd6440eb8d3dcace51929dbbccf94554b9a793dd
Veronika Folin <veronika.folin@studio.unibo.it>
2023-11-13 15:19:25 +0100

    feat: KPI to compute the customer retention rate

---
+++ models/marts/kpi/kpi_customer_retention_rate.sql
@@ -0,0 +1,36 @@
+{% set startPeriod = var("startPeriod") %}
+{% set endPeriod = var("endPeriod") %}
+
+with
+
+orders as (
+    select * from {{ref('stg_orders')}}
+),
+
+customers_beginning_of_period as (
+    select distinct custkey
+    from orders
+    where orderdate <= '{{startPeriod}}'
+),
+
+customers_end_of_period as (
+    select distinct custkey
+    from orders
+    where orderdate >= '{{startPeriod}}' and orderdate <= '{{endPeriod}}'
+),
+
+acquired_customers as (
+    select * from customers_end_of_period
+    except
+    select * from customers_beginning_of_period
+),
+
+final as (
+    select
+        (((select count(*) from customers_end_of_period) -
+        (select count(*) from acquired_customers)) /
+        (select count(*) from customers_beginning_of_period)) * 100
+        as customer_retention_rate
+)
+
+select * from final
