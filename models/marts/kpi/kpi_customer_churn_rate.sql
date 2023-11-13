045625c89847d0f90737a7fe09a3b4793530aeea
Veronika Folin <veronika.folin@studio.unibo.it>
2023-11-13 15:02:52 +0100

    feat: KPIs to compute the customer churn rate

---
+++ models/marts/kpi/kpi_customer_churn_rate.sql
@@ -0,0 +1,35 @@
+{% set startPeriod = var("startPeriod") %}
+{% set endPeriod = var("endPeriod") %}
+
+with
+
+orders as (
+    select * from {{ref('stg_orders')}}
+),
+
+customers_beginning_of_period as (
+    select distinct custkey
+    from orders
+    where orderdate <= '{{startPeriod}}'
+),
+
+customers_end_of_period as (
+    select distinct custkey
+    from orders
+    where orderdate >= '{{startPeriod}}' and orderdate <= '{{endPeriod}}'
+),
+
+lost_customers as (
+    select * from customers_beginning_of_period
+    except
+    select * from customers_end_of_period
+),
+
+final as (
+    select
+        (select count(lost_customers.*) from lost_customers)/
+        (select count(customers_beginning_of_period.*) from customers_beginning_of_period)
+        as customer_churn_rate
+)
+
+select * from final
