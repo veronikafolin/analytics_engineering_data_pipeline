97b1091e3a5dc8948604b71c9206c5a5a5b51dca
Veronika Folin <veronika.folin@studio.unibo.it>
2023-11-13 10:52:53 +0100

    refactor

--- models/marts/kpi/kpi_volume_sales_partsupp.sql
+++ models/marts/kpi/kpi_volume_sales_partsupp.sql
@@ -6,16 +6,14 @@ sales as (
 
 final as (
     select
-        suppkey,
-        partkey,
+        partsuppkey,
         min(orderdate) as first_order_date,
         max(orderdate) as most_recent_order_date,
         count(distinct orderkey) as number_of_orders,
         sum(quantity) as sum_of_quantity,
         sum(extendedprice) as total_revenue
     from sales
-    group by 1, 2
-    order by 1
+    group by 1
 )
 
 select * from final
