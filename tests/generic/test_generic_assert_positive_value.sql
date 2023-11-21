516c7c7d58530d77b8925d66fc9685ec20c0bb3e
Veronika Folin <veronika.folin@studio.unibo.it>
2023-11-21 09:46:01 +0100

    refactor tests

--- tests/generic/test_generic_assert_positive_value.sql
+++
@@ -1,6 +0,0 @@
-{% test test_generic_assert_positive_value(model, column_name) %}
-select
-   {{column_name}}
-from {{ model }}
-where {{column_name}} < 0
-{% endtest %}
--- tests/generic/test_generic_dates_constraints.sql
+++
@@ -1,5 +0,0 @@
-{% test test_generic_dates_constraints(model, column_name, next_date) %}
-select *
-from {{ model }}
-where {{column_name}} > {{next_date}}
-{% endtest %}
