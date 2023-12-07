f9f4f9b4ff693dd1e36550376d5106469a1aca37
Veronika Folin <veronika.folin@studio.unibo.it>
2023-12-07 18:15:31 +0100

    feat: configuration of DAG to generate dbt project's documentation

---
+++ dags/deploy.py
@@ -0,0 +1,20 @@
+from datetime import timedelta
+import airflow
+from airflow import DAG
+from airflow_dbt.operators.dbt_operator import (
+    DbtDocsGenerateOperator
+)
+
+default_args = {
+    'start_date': airflow.utils.dates.days_ago(0),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(dag_id='deploy_dbt_project', default_args=default_args, schedule_interval=None):
+
+    dbt_docs = DbtDocsGenerateOperator(
+        task_id='dbt_docs',
+    )
+
+    dbt_docs
