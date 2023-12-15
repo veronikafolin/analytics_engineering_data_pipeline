2d2e9481dbd9adbcc21a94067bbb8bdd80da54bf
Veronika Folin <veronika.folin@studio.unibo.it>
2023-12-15 19:12:00 +0100

    refactor

---
+++ dags/dag_factory_version/individuals_factory/utils.py
@@ -0,0 +1,10 @@
+from airflow.utils.state import State
+from dag_factory_version import common_utils
+
+
+def branch_func_on_customer_freshness(**kwargs):
+    task_status = common_utils.get_internal_task_state('check_customer_freshness', **kwargs)
+    if task_status == State.SUCCESS:
+        return 'trigger_nation_dag'
+    else:
+        return 'dbt_run_stg_customer'
--- dags/dag_factory_version/sales_factory/utils.py
+++ dags/dag_factory_version/sales_factory/utils.py
@@ -1,13 +1,26 @@
-from airflow.models import TaskInstance
 from airflow.utils.state import State
+from dag_factory_version import common_utils
 
 
 def branch_func_on_orders_freshness(**kwargs):
-    execution_date = kwargs['execution_date']
-    dag_instance = kwargs['dag']
-    operator_instance = dag_instance.get_task("check_orders_freshness")
-    task_status = TaskInstance(operator_instance, execution_date).current_state()
+    task_status = common_utils.get_internal_task_state('check_orders_freshness', **kwargs)
     if task_status == State.SUCCESS:
         return 'trigger_customer_dag'
     else:
         return 'dbt_run_stg_orders'
+
+
+# def get_execution_date_of_dim_customer():
+#     def get_last_execution_date_of_dim_customer(exec_date, **kwargs):
+#         dag_runs = DagRun.find(dag_id='dim_customer_2')
+#         dag_runs.sort(key=lambda x: x.execution_date, reverse=True)
+#         return dag_runs[0].execution_date if dag_runs else None
+#
+#     return get_last_execution_date_of_dim_customer
+
+def branch_func_on_dbt_test_dim_customer_state(**kwargs):
+    task_status = common_utils.get_external_task_state('dim_customer_2', 'dbt_test_dim_customer', **kwargs)
+    if task_status == State.SUCCESS:
+        return 'dbt_run_fct_orders'
+    else:
+        return None
