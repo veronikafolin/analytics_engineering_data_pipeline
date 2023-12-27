3450375abfb03c2e674f0ea08bf25a1435d5535b
Veronika Folin <veronika.folin@studio.unibo.it>
2023-12-27 18:06:13 +0100

    feat: check if an incremental table is initialized

--- dags/dag_factory_version/historical_version/individuals_factory/utils.py
+++ dags/dag_factory_version/historical_version/individuals_factory/utils.py
@@ -8,3 +8,11 @@ def branch_func_on_on_int_nation_existence(**kwargs):
         return 'dbt_run_registry_dim_customer'
     else:
         return 'trigger_nation_dag'
+
+
+def branch_func_on_on_registry_stg_customer_initialization(**kwargs):
+    task_status = common_utils.get_internal_task_state('check_registry_stg_customer_initialization', **kwargs)
+    if task_status == State.SUCCESS:
+        return 'dbt_run_registry_stg_customer'
+    else:
+        return 'dbt_run_registry_stg_customer_full_refresh'
