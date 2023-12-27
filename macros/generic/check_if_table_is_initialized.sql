07f7bf3147245244783f738f083da0d21dd31837
Veronika Folin <veronika.folin@studio.unibo.it>
2023-12-27 17:25:53 +0100

    feat: check if an incremental table is initialized

--- macros/generic/check_if_table_is_initialized.sql
+++
@@ -1,8 +0,0 @@
-{% macro check_if_table_is_initialized(table_name) %}
-
-SELECT ROW_COUNT
-FROM ANALYTICS.INFORMATION_SCHEMA.TABLES
-WHERE  table_schema = 'DBT_CORE_REUSABLE_DEMO'
-AND    table_name   = {{table_name}}
-
-{% endmacro %}
