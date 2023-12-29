1d9e49ad3eeb2a433ad3195999ee338a122ca510
Veronika Folin <veronika.folin@studio.unibo.it>
2023-12-29 16:31:57 +0100

    refactor

--- dags/dag_factory_version/current/sales_factory/utils.py
+++ dags/dag_factory_version/current/sales_factory/utils.py
@@ -10,14 +10,6 @@ def branch_func_on_orders_freshness(**kwargs):
         return 'dbt_run_stg_orders'
 
 
-# def get_execution_date_of_dim_customer():
-#     def get_last_execution_date_of_dim_customer(exec_date, **kwargs):
-#         dag_runs = DagRun.find(dag_id='dim_customer_2')
-#         dag_runs.sort(key=lambda x: x.execution_date, reverse=True)
-#         return dag_runs[0].execution_date if dag_runs else None
-#
-#     return get_last_execution_date_of_dim_customer
-
 def branch_func_on_dbt_test_dim_customer_state(**kwargs):
     task_status = common_utils.get_external_task_state('dim_customer_2', 'dbt_test_dim_customer', **kwargs)
     if task_status == State.SUCCESS:
--- dags/dag_factory_version/flat_version/dashboard_factory/dashboard.py
+++
@@ -1,6 +0,0 @@
-from dagfactory import DagFactory
-
-dag_factory = DagFactory("/home/airflow/gcs/dags/dag_factory_version/flat_version/dashboard_factory/config.yml")
-
-dag_factory.clean_dags(globals())
-dag_factory.generate_dags(globals())
