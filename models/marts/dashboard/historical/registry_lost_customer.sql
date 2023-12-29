119652496f063362681acd8d0f2424802a65e64f
Veronika Folin <veronika.folin@studio.unibo.it>
2023-12-29 16:18:15 +0100

    feat: registry tables for dashboard models

---
+++ models/marts/dashboard/historical/registry_lost_customer.sql
@@ -0,0 +1,45 @@
+{% set startPeriod = var("startPeriod") %}
+{% set endPeriod = var("endPeriod") %}
+
+with
+
+sales as (
+    select *
+    from {{ref('registry_fct_sales')}}
+    {{ apply_partition_date() }}
+),
+
+filtered_sales as (
+    select *
+    from sales
+    {{ write_where_by_vars() }}
+),
+
+customers_beginning_of_period as (
+    select distinct custkey
+    from filtered_sales
+    where orderdate <= '{{startPeriod}}'
+),
+
+customers_end_of_period as (
+    select distinct custkey
+    from filtered_sales
+    where orderdate >= '{{startPeriod}}' and orderdate <= '{{endPeriod}}'
+),
+
+lost_customers as (
+    select * from customers_beginning_of_period
+    except
+    select * from customers_end_of_period
+),
+
+final as (
+    select
+        {{ write_select_groupByColumns_by_vars() }}
+        count(custkey) as count_of_lost_customers
+    from lost_customers
+    join filtered_sales using(custkey)
+    {{ write_groupBY_groupByColumns_by_vars() }}
+)
+
+select * from final
---
+++ models/marts/dashboard/historical/registry_sales_time.sql
@@ -0,0 +1,20 @@
+with
+
+sales as (
+    select *
+    from {{ref('registry_fct_sales')}}
+    {{ apply_partition_date() }}
+),
+
+final as (
+    select
+        {{ write_select_groupByColumns_by_vars() }}
+        count(distinct orderkey) as number_of_orders,
+        avg(datediff(day, shipdate, receiptdate)) as avg_delivery_days,
+        avg(datediff(day, orderdate, receiptdate)) as avg_fulfillment_days
+    from sales
+    {{ write_where_by_vars() }}
+    {{ write_groupBY_groupByColumns_by_vars() }}
+)
+
+select * from final
