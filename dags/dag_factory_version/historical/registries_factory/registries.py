cbd2038ef7505a14e2d9350bf39dd63ce87df817
Veronika Folin <veronika.folin@studio.unibo.it>
2024-01-04 09:09:34 +0100

    refactor: decomposition of DAGs

---
+++ dags/dag_factory_version/historical/registries_factory/registries.py
@@ -0,0 +1,6 @@
+from dagfactory import DagFactory
+
+dag_factory = DagFactory("/home/airflow/gcs/dags/dag_factory_version/historical/registries_factory/config.yml")
+
+dag_factory.clean_dags(globals())
+dag_factory.generate_dags(globals())
