fb2ed6a7db64490097a9fef97392ad375830cb71
Veronika Folin <veronika.folin@studio.unibo.it>
2024-01-04 08:46:54 +0100

    refactor: decomposition of DAGs

--- dags/dag_factory_version/historical/individuals_factory/utils.py
+++ dags/dag_factory_version/historical/individuals_factory/utils.py
@@ -2,22 +2,6 @@ from airflow.utils.state import State
 import common_utils
 
 
-def branch_func_customer_on_int_nation_existence(**kwargs):
-    task_status = common_utils.get_internal_task_state('check_int_nation_existence', **kwargs)
-    if task_status == State.SUCCESS:
-        return 'dbt_test_registry_stg_customer'
-    else:
-        return 'trigger_nation_dag'
-
-
-def branch_func_supplier_on_int_nation_existence(**kwargs):
-    task_status = common_utils.get_internal_task_state('check_int_nation_existence', **kwargs)
-    if task_status == State.SUCCESS:
-        return 'dbt_test_registry_stg_supplier'
-    else:
-        return 'trigger_nation_dag'
-
-
 def branch_func_on_registry_stg_customer_initialization(**kwargs):
     task_status = common_utils.get_internal_task_state('check_registry_stg_customer_initialization', **kwargs)
     if task_status == State.SUCCESS:
---
+++ dags/dag_factory_version/historical/places_factory/utils.py
@@ -0,0 +1,10 @@
+from airflow.utils.state import State
+import common_utils
+
+
+def branch_func_on_int_nation_existence(**kwargs):
+    task_status = common_utils.get_internal_task_state('check_int_nation_existence', **kwargs)
+    if task_status == State.SUCCESS:
+        return None
+    else:
+        return 'dbt_run_stg_nation'
