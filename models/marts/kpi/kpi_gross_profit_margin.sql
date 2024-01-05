f4c83b781cc3d304773a37a4b7fd6eea8529e909
Veronika Folin <veronika.folin@studio.unibo.it>
2024-01-05 15:20:19 +0100

    refactor

--- models/marts/kpi/kpi_gross_profit_margin.sql
+++ models/marts/kpi/kpi_gross_profit_margin.sql
@@ -16,21 +16,6 @@ filtered_sales as (
     {{ write_where_by_vars() }}
 ),
 
---profit as (
---    select
---        filtered_sales.discounted_extended_price as revenue,
---        (inventory.supplycost * sales.quantity) as cost_of_good_sold,
---        (revenue - cost_of_good_sold) as profit
---    from filtered_sales
---    join inventory using(partsuppkey)
---),
---
---final as (
---    select
---        ((sum(revenue) - sum(cost_of_good_sold))/sum(revenue)) * 100 as gross_profit_margin
---    from profit
---)
-
 final as (
     select
         {{ write_select_groupByColumns_by_vars_from_table('filtered_sales') }}
