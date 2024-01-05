ba426e1101c50d6d3b6bc99b175df8fd1baf293b
Veronika Folin <veronika.folin@studio.unibo.it>
2024-01-05 15:14:09 +0100

    feat: to compute kpi_customer_segmentation

--- models/marts/kpi/kpi_customer_segmentation.sql
+++ models/marts/kpi/kpi_customer_segmentation.sql
@@ -1,15 +1,15 @@
 with
 
-customers as (
-    select * from {{ref('registry_dim_customer')}}
+sales as (
+    select * from {{ref('registry_fct_sales')}}
     {{ apply_partition_date() }}
 ),
 
 final as (
     select
         {{ write_select_groupByColumns_by_vars() }}
-        count(custkey) as number_of_customers
-    from customers
+        count(distinct custkey) as number_of_customers
+    from sales
     {{ write_where_by_vars() }}
     {{ write_groupBY_groupByColumns_by_vars() }}
 )
