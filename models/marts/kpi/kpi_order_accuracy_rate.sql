d6f7e70423e608e4ce9b5c3b1f4b04eeebb4cc7b
Veronika Folin <veronika.folin@studio.unibo.it>
2024-01-05 14:21:37 +0100

    feat: to compute and check kpi_order_accuracy_rate

--- models/marts/kpi/kpi_order_accuracy_rate.sql
+++ models/marts/kpi/kpi_order_accuracy_rate.sql
@@ -9,20 +9,39 @@ filtered_orders as (
     select *
     from orders
     {{ write_where_by_vars() }}
+),
+
+grouped_filtered_orders as (
+    select
+       {{ write_select_groupByColumns_by_vars() }}
+       count(*) as number_of_orders
+    from filtered_orders
     {{ write_groupBY_groupByColumns_by_vars() }}
+
 ),
 
 fulfilled_orders as (
-    select orderkey
+    select *
     from filtered_orders
     where orderstatus = 'F'
 ),
 
+grouped_fulfilled_orders as (
+    select
+        {{ write_select_groupByColumns_by_vars() }}
+        count(*) as number_of_fulfilled_orders
+    from fulfilled_orders
+    {{ write_groupBY_groupByColumns_by_vars() }}
+),
+
 final as (
     select
-    ((select count(orderkey) from fulfilled_orders) /
-    (select count(orderkey) from orders)) * 100
-    as order_accuracy_rate
+        {{ write_select_groupByColumns_by_vars_from_table('grouped_filtered_orders') }}
+        CAST(avg(grouped_filtered_orders.number_of_orders) AS INT) as number_of_orders,
+        CAST(avg(grouped_fulfilled_orders.number_of_fulfilled_orders) AS INT) as number_of_fulfilled_orders,
+        CAST(avg((number_of_fulfilled_orders/number_of_orders)*100) AS DECIMAL(10,2)) as order_accuracy_rate
+    from grouped_filtered_orders INNER JOIN grouped_fulfilled_orders
+    {{ write_groupBY_groupByColumns_by_vars_from_table('grouped_filtered_orders') }}
 )
 
 select * from final
