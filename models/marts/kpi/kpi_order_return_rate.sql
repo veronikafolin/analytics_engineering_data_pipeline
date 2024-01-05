ebe83dc07a39423d84e63ae20bce8dedefc56977
Veronika Folin <veronika.folin@studio.unibo.it>
2024-01-05 10:25:21 +0100

    refactor: join

--- models/marts/kpi/kpi_order_return_rate.sql
+++ models/marts/kpi/kpi_order_return_rate.sql
@@ -39,7 +39,7 @@ final as (
         grouped_filtered_sales.number_of_sales,
         grouped_returned_sales.number_of_returned_sales,
         (number_of_returned_sales/number_of_sales)*100 as order_return_rate
-    from grouped_filtered_sales INNER JOIN grouped_returned_sales
+    from grouped_filtered_sales LEFT JOIN grouped_returned_sales
     {{ write_groupBY_groupByColumns_by_vars() }}
 )
 
