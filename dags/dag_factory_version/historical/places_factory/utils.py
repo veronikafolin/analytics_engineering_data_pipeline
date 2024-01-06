3d3f694022d52cd425c206f0c0dbbf527b8585ae
Veronika Folin <veronika.folin@studio.unibo.it>
2024-01-06 08:41:27 +0100

    refactor: DummyOperator on branch

--- dags/dag_factory_version/historical/places_factory/utils.py
+++ dags/dag_factory_version/historical/places_factory/utils.py
@@ -5,6 +5,6 @@ import common_utils
 def branch_func_on_int_nation_existence(**kwargs):
     task_status = common_utils.get_internal_task_state('check_int_nation_existence', **kwargs)
     if task_status == State.SUCCESS:
-        return None
+        return ['nothing_to_do']
     else:
         return ['dbt_run_stg_region']
