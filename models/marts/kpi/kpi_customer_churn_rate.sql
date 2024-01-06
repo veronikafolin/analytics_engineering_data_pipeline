60310048fb7b6fe5a3ba340a1ee7fe2dcadd49ec
Veronika Folin <veronika.folin@studio.unibo.it>
2024-01-06 15:02:53 +0100

    feat: to compute and check kpi_customer_churn_rate

--- models/marts/kpi/kpi_customer_churn_rate.sql
+++ models/marts/kpi/kpi_customer_churn_rate.sql
@@ -1,6 +1,3 @@
-{% set startPeriod = var("startPeriod") %}
-{% set endPeriod = var("endPeriod") %}
-
 with
 
 orders as (
@@ -12,19 +9,34 @@ orders_filtered as (
     select *
     from orders
     {{ write_where_by_vars() }}
-    {{ write_groupBY_groupByColumns_by_vars() }}
 ),
 
 customers_beginning_of_period as (
     select distinct custkey
     from orders_filtered
-    where orderdate <= '{{startPeriod}}'
+    where orderdate <= '{{var("startPeriod")}}'
+),
+
+grouped_customers_beginning_of_period as (
+    select
+        {{ write_select_groupByColumns_by_vars() }}
+        count(distinct custkey) as customers_beginning_of_period
+    from customers_beginning_of_period left join orders_filtered using (custkey)
+    {{ write_groupBY_groupByColumns_by_vars() }}
 ),
 
 customers_end_of_period as (
     select distinct custkey
     from orders_filtered
-    where orderdate >= '{{startPeriod}}' and orderdate <= '{{endPeriod}}'
+    where orderdate >= '{{var("startPeriod")}}' and orderdate <= '{{var("endPeriod")}}'
+),
+
+grouped_customers_end_of_period as (
+    select
+        {{ write_select_groupByColumns_by_vars() }}
+        count(distinct custkey) as customers_end_of_period
+    from customers_end_of_period left join orders_filtered using (custkey)
+    {{ write_groupBY_groupByColumns_by_vars() }}
 ),
 
 lost_customers as (
@@ -33,11 +45,26 @@ lost_customers as (
     select * from customers_end_of_period
 ),
 
+grouped_lost_customers as (
+    select
+        {{ write_select_groupByColumns_by_vars() }}
+        count(distinct custkey) as lost_customers
+    from lost_customers left join orders_filtered using (custkey)
+    {{ write_groupBY_groupByColumns_by_vars() }}
+),
+
 final as (
     select
-        ((select count(*) from lost_customers)/
-        (select count(*) from customers_beginning_of_period)) * 100
+        {{ write_select_groupByColumns_by_vars_from_table('grouped_lost_customers') }}
+        CAST(avg(grouped_customers_beginning_of_period.customers_beginning_of_period) AS INT) as customers_beginning_of_period,
+        CAST(avg(grouped_customers_end_of_period.customers_end_of_period) AS INT) as customers_end_of_period,
+        CAST(avg(grouped_lost_customers.lost_customers) AS INT) as lost_customers,
+        CAST(avg(lost_customers/customers_beginning_of_period) * 100  AS DECIMAL(10,2))
         as customer_churn_rate
+    from grouped_customers_beginning_of_period
+    join grouped_customers_end_of_period
+    join grouped_lost_customers
+    {{ write_groupBY_groupByColumns_by_vars_from_table('grouped_lost_customers') }}
 )
 
 select * from final
