4e5f308ee44d11c35b66ae9c9a5d9af380a6e014
Veronika Folin <veronika.folin@studio.unibo.it>
2024-01-11 10:52:57 +0100

    refactor: project's name

--- models/staging/dataquality/metadata_test.sql
+++ models/staging/dataquality/metadata_test.sql
@@ -7,7 +7,7 @@ metadata_tables as (
         LOWER(table_name) as table_name,
         row_count
     from {{ source('metadata', 'tables') }}
-    where table_catalog = 'ANALYTICS' and table_schema = 'DBT_CORE_REUSABLE_DEMO' and table_type = 'BASE TABLE'
+    where table_catalog = 'ANALYTICS' and table_schema = 'ANALYTICS_ENGINEERING_DATA_PIPELINE' and table_type = 'BASE TABLE'
 )
 
 select * from metadata_tables
